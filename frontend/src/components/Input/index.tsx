3ea5cedfc7bfd2d99260ade32ce5c963f7222f48
Gleb Voitenko <v8tenko@gmail.com>
2022-07-09 19:40:18 +0200

    feat: login page

--- frontend/src/components/Input/index.tsx
+++ frontend/src/components/Input/index.tsx
@@ -1,3 +1,43 @@
-export const Input = () => {
-	return <input />;
+import { useCallback, useState } from 'react';
+
+import { bem } from '@utils';
+
+import styles from './Input.module.css';
+
+type Props = {
+	initial?: string;
+	className?: string;
+	label?: string;
+	name: string;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+export const Input: React.FC<Props> = ({ initial, onChange, name, className, label, ...rest }) => {
+	const [content, setContent] = useState(initial || '');
+
+	const handleInputChanged = useCallback(
+		(e: React.ChangeEvent<HTMLInputElement>) => {
+			onChange(e);
+			setContent(e.target.value);
+		},
+		[onChange]
+	);
+
+	return (
+		<div className={bem(className, styles.container)}>
+			<input
+				name={name}
+				id={name}
+				value={content}
+				onChange={handleInputChanged}
+				className={styles.input}
+				{...rest}
+			/>
+			{label && (
+				<label className={styles.label} htmlFor={name}>
+					{label}
+				</label>
+			)}
+		</div>
+	);
 };
--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,12 +1,43 @@
-import { Input } from '@components';
+import { useCallback, useState } from 'react';
+
+import { Button, Input } from '@components';
+import { noop } from '@utils';
 
 import styles from './LoginPage.module.css';
 
+type LoginParameters = {
+	username?: string;
+	password?: string;
+};
+
 export const LoginPage: React.FC = () => {
+	const [loginForm, setLoginForm] = useState<LoginParameters>({});
+	const updateForm = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+		setLoginForm((oldForm: LoginParameters) => ({ ...oldForm, [e.target.name]: e.target.value }));
+	}, []);
+
+	const validateForm = () => {
+		const { username, password } = loginForm;
+
+		return (username?.length || 0) >= 5 && (password?.length || 0) >= 8;
+	};
+
 	return (
 		<div className={styles.container}>
-			<p> Simple messenger </p>
-			<Input />
+			<div className={styles.texts}>
+				<h1> Simple messenger </h1>
+				<p className={styles.description}>
+					Please, enter your username and password <br />
+					to login unto the user
+				</p>
+			</div>
+			<div className={styles.loginInputs}>
+				<Input name="username" label="Your username" onChange={updateForm} />
+				<Input name="password" label="Password" type="password" onChange={updateForm} />
+			</div>
+			<Button className={validateForm() ? styles.showButton : styles.hideButton} onClick={noop} disabled={true}>
+				NEXT
+			</Button>
 		</div>
 	);
 };
